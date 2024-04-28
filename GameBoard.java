fa4b480308ae79ed66c00a8ba467b892efebce84
C1nner <con.bru001@gmail.com>
2024-04-28 15:15:06 -0700

    started game board

---
+++ GameBoard.java
@@ -0,0 +1,123 @@
+import javax.swing.*;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class GameBoard {
+    Thread print;
+    static int tSpeed;
+    static final int SYSTEM_WIDTH = 400;
+    static final int SYSTEM_HEIGHT = 640;
+    static final int SYSTEM_BORDER = SYSTEM_WIDTH / 10;
+    static final int MAX_CHAR = SYSTEM_WIDTH / 10;
+    static final JLabel LABEL = new JLabel();
+    static final JTextPane TEXT1 = new JTextPane();
+    static final JTextField INPUT = new JTextField(10);
+    static final JFrame SYSTEM = new JFrame("NBES (Non Binary Entertainment System)");
+
+    String lastsPrint = "";
+    volatile boolean keyButton = false;
+
+    private Deck deck;
+    private Character[] team = new Character[3];
+
+    public GameBoard(Deck cards) {
+        deck = cards;
+        try {
+            GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(Font.createFont(Font.TRUETYPE_FONT, new File("Files/Retro Gaming.ttf")));
+            TEXT1.setFont(new Font("Retro Gaming", Font.BOLD, 12));
+        } catch (IOException | FontFormatException ignored) {
+        }
+        LABEL.setLayout(new FlowLayout(FlowLayout.CENTER));
+        StyledDocument style = TEXT1.getStyledDocument();
+        SimpleAttributeSet align = new SimpleAttributeSet();
+        StyleConstants.setAlignment(align, StyleConstants.ALIGN_CENTER);
+        style.setParagraphAttributes(0, style.getLength(), align, false);
+        TEXT1.setEditable(false);
+        TEXT1.setBorder(null);
+        TEXT1.setOpaque(false);
+        TEXT1.setForeground(Color.BLACK);
+        TEXT1.setSize(SYSTEM_WIDTH - SYSTEM_BORDER * 2, SYSTEM_HEIGHT);
+
+        LABEL.add(TEXT1);
+        INPUT.setEditable(false);
+        SYSTEM.setResizable(false);
+        SYSTEM.add(INPUT, BorderLayout.SOUTH);
+        SYSTEM.add(LABEL);
+        SYSTEM.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        SYSTEM.setVisible(true);
+        LABEL.setIcon(new ImageIcon(new BufferedImage(SYSTEM_WIDTH, SYSTEM_HEIGHT, BufferedImage.TYPE_INT_ARGB)));
+        SYSTEM.pack();
+    }
+    public void sPrintln(String str) {
+        TEXT1.setText("");
+        AtomicReference<String> text = new AtomicReference<>("");
+        String finalStr = textFormat(str);;
+        print = new Thread(() -> {
+            for (int i = 0; i < finalStr.length(); i++) {
+                int finalI = i;
+                text.updateAndGet(v -> v + finalStr.charAt(finalI));
+                TEXT1.setText(text.get());
+            }
+            TEXT1.setText(text + "\n>Click<");
+        });
+
+        SYSTEM.requestFocusInWindow();
+        keyButton = false;
+        print.start();
+        while (!keyButton) ;
+        print.stop();
+
+        keyButton = false;
+        TEXT1.setText("");
+        lastsPrint = "";
+    }
+    public String textFormat(String str) {
+        StringBuilder string = new StringBuilder();
+        char[] charArr = str.toCharArray();
+        int reset = 0;
+        for (char c : charArr) {
+            if ((c + "").equals("\n")) {
+                reset = 0;
+            } else if (reset == MAX_CHAR) {
+                string.append("\n");
+                reset = 0;
+            }
+            string.append(c);
+            reset++;
+        }
+        return string.toString();
+    }
+    public int choice(String str) {
+        INPUT.setText("");
+        INPUT.setEditable(true);
+        INPUT.requestFocus();
+        keyButton = false;
+        TEXT1.setText(str+" (Press number on your keyboard corresponding to the number you want 2 times");
+        while(formatInput(INPUT.getText())==0);
+        TEXT1.setText("");
+        return formatInput(INPUT.getText());
+    }
+    public int formatInput(String str) {
+        char[] chars = str.toCharArray();
+        if(str.length()>2 && strIsInt(str) && chars[chars.length-1]==chars[chars.length-2] ) {
+            return chars[chars.length-1];
+        }
+        return 0;
+    }
+    public static boolean strIsInt(String string) {
+        try {
+            Integer.parseInt(string);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+}
--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
+        new GameBoard(new Deck((new Card[] {new Card("",false)})));
     }
 }
